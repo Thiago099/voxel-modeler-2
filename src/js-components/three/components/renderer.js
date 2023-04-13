49f6256b0690187fc73873128bf07d3bc5173325
Thiago <thiago.kaique@stayfilm.com.br>
2023-04-12 22:12:27 -0300

    SSAO

--- src/js-components/three/components/renderer.js
+++ src/js-components/three/components/renderer.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { canvasResize } from '../lib/canvas-resize.js'
 export { CreateRenderer }
+
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { SSAOPass } from 'three/addons/postprocessing/SSAOPass.js';
 function CreateRenderer(canvas,orbit)
 {
     const renderer = new THREE.WebGLRenderer({
@@ -9,6 +12,10 @@ function CreateRenderer(canvas,orbit)
     });
 
 
+
+
+
+    
 
 
     renderer.shadowMap.enabled = true;
@@ -23,30 +30,48 @@ function CreateRenderer(canvas,orbit)
 
 
 
-
-
-
-
     renderer.setPixelRatio( window.devicePixelRatio );
     
     renderer.setClearColor( 0xffffff, 0);
 
     const scene = new THREE.Scene();
 
+    var composer = new EffectComposer( renderer );
+
+
+
+
+    const ssaoPass = new SSAOPass( scene, orbit.camera, 1000, 1000 );
+    ssaoPass.kernelRadius = 1;
+    ssaoPass.minDistance = 1e-10;
+    ssaoPass.maxDistance = 0.1;
+    //quality
+    
+
+
+    composer.addPass( ssaoPass );
+
+    ssaoPass.output = SSAOPass.OUTPUT.Default;
+
     
     canvasResize(canvas,(width,height)=>{
         renderer.setSize( width, height );
         orbit.camera.aspect = width / height;
         orbit.camera.updateProjectionMatrix();
+        composer.setSize( width, height );
+        ssaoPass.setSize( width, height );
     })
 
     function render()
     {
-        renderer.render( scene, orbit.camera );
+        composer.render();
     }
     function add(object)
     {
         scene.add(object)
+
+    
+        
     }
 
     return {render, add}
